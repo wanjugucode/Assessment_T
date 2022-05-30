38f46b8ad084d8a3c6572614111aa1477bec2c4c
unknown <wanjugucode>
2022-05-30 13:01:49 +0300

    assessment

--- assessment.py
+++ assessment.py
@@ -2,17 +2,22 @@
 # function that holds the calculations
 # area of a circle or square
 
-# area of a squre= length * width
+# area of a rectangle= length * width
 
 # python
 
 
 
 Class Calculations:
-    def __init__(self):
+    def __init__(self,length,width):
+        self.side=side
+        self.width=width
         
-        def area(arrg):
-            for i in arrg:
-                if i>0
-                x=i*i
-                
+    def area():
+        myCal=self.length*self.width
+        return myCal
+cal=Calculations(4,6)
+print(cal)
+
+
+
