4f8ddc3dd6bd6cde18115062da8f58bcc7dfb4aa
unknown <wanjugucode>
2022-05-30 12:19:51 +0300

    assessment

---
+++ assessment.py
@@ -0,0 +1,18 @@
+# class
+# function that holds the calculations
+# area of a circle or square
+
+# area of a squre= length * width
+
+# python
+
+
+
+Class Calculations:
+    def __init__(self):
+        
+        def area(arrg):
+            for i in arrg:
+                if i>0
+                x=i*i
+                
